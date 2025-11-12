89e20b5bb8843a073dd1b00f3754a4d81f31b576
박지민 <pjmlove2008@gmail.com>
2025-11-12 18:54:00 +0900

    ✨: add errorCode for BadRequest

--- src/main/kotlin/wee/sh/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/wee/sh/global/error/exception/ErrorCode.kt
@@ -7,6 +7,7 @@ enum class ErrorCode(
     val status: Int,
     val message: String
 ) {
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error")
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    BadRequest(400, "Bad Request"),
     ;
 }

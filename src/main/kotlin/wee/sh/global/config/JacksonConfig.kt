ad10cee3a215431de2fbddeb07432c68232a67f7
박지민 <pjmlove2008@gmail.com>
2025-11-13 20:20:33 +0900

    ✨: timestamp

--- src/main/kotlin/wee/sh/global/config/JacksonConfig.kt
+++ src/main/kotlin/wee/sh/global/config/JacksonConfig.kt
@@ -2,6 +2,7 @@ package wee.sh.global.config
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
@@ -19,6 +20,7 @@ class JacksonConfig {
             registerKotlinModule()
             registerModule(JavaTimeModule())
             setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
     }
 }
